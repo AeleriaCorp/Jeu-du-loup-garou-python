6fd93f7d7185713d194cf7e4dad4278c5006a575
Bro-vin <40148651+Bro-vin@users.noreply.github.com>
2018-06-11 14:21:52 +0200

    indentation

--- loup garou.py
+++ loup garou.py
@@ -49,16 +49,16 @@ class Loup:
     def assign_capitaine(self,votes):
     '''assigne le role de capitaine en fonction des resultat du votes
     vote = {pseudo:vote}'''
-    maxi = []
-    for i in votes.keys():
-        if votes[i] >= max(votes.values()) :
-            maxi.append(i)
+        maxi = []
+        for i in votes.keys():
+            if votes[i] >= max(votes.values()) :
+                maxi.append(i)
 
-    if len(maxi)>1:
-        a = randint(0,len(maxi)-1)
-        joueur = maxi[a]
+        if len(maxi)>1:
+            a = randint(0,len(maxi)-1)
+            joueur = maxi[a]
 
-    return joueur
+        return joueur
 
     def design_capitaine(self,capitaine):
         '''designe le nouveau capitaine'''
