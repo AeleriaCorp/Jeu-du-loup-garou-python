8eb2ed2e271f6d546f5d98210fc435353109ab7e
Bro-vin <40148651+Bro-vin@users.noreply.github.com>
2018-06-11 12:53:12 +0200

    j
    e

--- jour.py
+++ jour.py
@@ -1,1 +1,16 @@
+from random import *
 
+def assign_capitaine(votes):
+    '''assigne le role de capitaine en fonction des resultat du votes
+    vote = {pseudo:vote}'''
+    maxi = []
+    for i in votes.keys():
+        if votes[i] >= max(votes.values()) :
+            maxi.append(i)
+
+    if len(maxi)>1:
+        a = randint(0,len(maxi)-1)
+        joueur = maxi[a]
+        
+    return joueur
+        
