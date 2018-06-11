b4b6e9bffb12f356873bceb3bf16b2fadd767e71
Aeiko <36745896+Aeyko@users.noreply.github.com>
2018-06-11 11:45:32 +0200

    doc init et liste role
    ..

--- loup garou.py
+++ loup garou.py
@@ -1,4 +1,6 @@
 
 class Loup:
+
     def __init__(self,nb_player,pseudos):
-        
+            '''Initialisation du loup garou'''
+            role=[mj,loup,loup,voyante,sorciere,voleur,chasseur,cupidon]
